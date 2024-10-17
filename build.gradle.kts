28299375393948d48e29b8cd32c7c00a2bf6c6f1
Jason <141192101+jyod27@users.noreply.github.com>
2024-10-17 15:10:01 -0400

    Adds files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
-group = "org.example"
+group = "edu.bsu.cs"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -12,8 +14,24 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.jayway.jsonpath:json-path:2.9.0")
+    implementation("net.minidev", "json-smart", "2.5.0")
+    implementation("org.slf4j", "slf4j-nop", "2.0.11")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+javafx {
+    version = "22"
+    modules("javafx.controls", "javafx.fxml")
+}
+
+application {
+    mainClass.set("edu.bsu.cs.WaitlistGUI")
+}
+
+tasks.withType<JavaExec> {
+    standardInput = System.`in`
 }
