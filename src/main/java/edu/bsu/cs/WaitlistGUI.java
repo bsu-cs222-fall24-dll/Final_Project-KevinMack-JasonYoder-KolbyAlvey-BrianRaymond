cb990b0e6d64610e1d772448e885f5e19b2cf33f
Jason <141192101+jyod27@users.noreply.github.com>
2024-10-17 16:41:37 -0400

    Starts work on initial GUI class

--- src/main/java/edu/bsu/cs/WaitlistGUI.java
+++ src/main/java/edu/bsu/cs/WaitlistGUI.java
@@ -1,4 +1,10 @@
 package edu.bsu.cs;
 
+import static javafx.application.Application.launch;
+
 public class WaitlistGUI {
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
 }
