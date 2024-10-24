d4e16a2d5b08e171dd011f9bfeaa3291093463b2
kevin <kevin.mack@bsu.edu>
2024-10-24 15:59:01 -0400

    new items

--- src/test/java/edu/bsu/cs/PartyManagerTest.java
+++ src/test/java/edu/bsu/cs/PartyManagerTest.java
@@ -21,38 +21,41 @@ public class PartyManagerTest {
     @AfterEach
     public void tearDown() {
         testVBox.getChildren().clear();
-        Platform.runLater(() -> {
-        });
+        Platform.runLater(() -> {});
     }
 
     @Test
     public void testAddParty() {
-        partyManager.addParty("12", "kevin", "098-765-4321", "5 mins");
+        partyManager.addParty(4, "kevin", "098-765-4321", 5);
 
         assertEquals(1, testVBox.getChildren().size(), "Party should be added to the VBox.");
 
-        HBox partyHBox = (HBox) testVBox.getChildren().getFirst();
+        HBox partyHBox = (HBox) testVBox.getChildren().get(0);
         assertNotNull(partyHBox, "Party HBox should not be null.");
         assertEquals("4", ((TextField) partyHBox.getChildren().get(0)).getText(), "Size should match.");
-        assertEquals("Johnnyboy", ((TextField) partyHBox.getChildren().get(1)).getText(), "Name should match.");
-        assertEquals("317-723-8759", ((TextField) partyHBox.getChildren().get(2)).getText(), "Phone should match.");
-        assertEquals("32 mins", ((TextField) partyHBox.getChildren().get(3)).getText(), "Wait time should match.");
+        assertEquals("kevin", ((TextField) partyHBox.getChildren().get(1)).getText(), "Name should match.");
+        assertEquals("098-765-4321", ((TextField) partyHBox.getChildren().get(2)).getText(), "Phone should match.");
+        assertEquals("5", ((TextField) partyHBox.getChildren().get(3)).getText(), "Wait time should match.");
     }
 
     @Test
     public void testRemoveParty() {
-        partyManager.addParty("4", "johnnyboy", "317-723-8759", "32 mins");
-        partyManager.addParty("12", "kevin", "098-765-4321", "5 mins");
+        partyManager.addParty(4, "johnnyboy", "317-723-8759", 32);
+        partyManager.addParty(12, "kevin", "098-765-4321", 5);
+
 
         partyManager.removeParty();
 
         assertEquals(1, testVBox.getChildren().size(), "One party should remain in the VBox.");
+        HBox remainingPartyHBox = (HBox) testVBox.getChildren().get(0);
+        assertEquals("kevin", ((TextField) remainingPartyHBox.getChildren().get(1)).getText(), "Remaining party's name should be 'kevin'.");
     }
 
     @Test
     public void testRemoveNonExistentParty() {
-        partyManager.addParty("4", "johnnyboy", "317-723-8759", "32 mins");
+        partyManager.addParty(4, "johnnyboy", "317-723-8759", 32);
         partyManager.removeParty();
+
         assertEquals(0, testVBox.getChildren().size(), "VBox should be empty after removal.");
     }
 }
