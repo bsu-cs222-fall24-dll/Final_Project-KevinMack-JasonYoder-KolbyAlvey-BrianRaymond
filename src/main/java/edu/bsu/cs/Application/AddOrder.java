df3eeeac35c112ed70030ab8aa0889abda01ce85
Jason <141192101+jyod27@users.noreply.github.com>
2024-11-21 16:15:37 -0500

    Improves adding items to orders

--- src/main/java/edu/bsu/cs/Application/AddOrder.java
+++ src/main/java/edu/bsu/cs/Application/AddOrder.java
@@ -13,10 +13,10 @@ import java.util.Objects;
 
 public class AddOrder {
 
-    private static final String[] MENUITEMS = {"Cheeseburger", "Bacon Burger", "Ham and Cheese",
-            "Chicken Sandwich", "Cobb Salad", "Chicken Tenders", "Straight Fries",
-            "Curly Fries", "Mac n Cheese", "Side Salad", "Fruit Plate", "Applesauce",
-            "Fountain Drink", "Lemonade", "Water"};
+    private static final String[] MENUITEMS = {"cheeseburger", "bacon-burger", "ham-and-cheese",
+            "chicken-sandwich", "cobb-salad", "chicken-tenders", "straight-fries",
+            "curly-fries", "mac-n-cheese", "side-salad", "fruit-plate", "applesauce",
+            "fountain-drink", "lemonade", "water"};
 
     public void showAddOrder() {
         Stage orderStage = new Stage();
@@ -42,37 +42,10 @@ public class AddOrder {
         TextArea orderBox = (TextArea) orderScreen.lookup("#orderBox");
         TextField specialInstructionsField = (TextField) orderScreen.lookup("#specialInstructions");
 
-        Button cheeseburgerButton = (Button) orderScreen.lookup("#cheeseburger");
-        Button baconBurgerButton = (Button) orderScreen.lookup("#baconBurger");
-        Button hamAndCheeseButton = (Button) orderScreen.lookup("#hamAndCheese");
-        Button chickenSandwichButton = (Button) orderScreen.lookup("#chickenSandwich");
-        Button cobbSaladButton = (Button) orderScreen.lookup("#cobbSalad");
-        Button chickenTendersButton = (Button) orderScreen.lookup("#chickenTenders");
-        Button straightFriesButton = (Button) orderScreen.lookup("#straightFries");
-        Button curlyFriesButton = (Button) orderScreen.lookup("#curlyFries");
-        Button macNCheeseButton = (Button) orderScreen.lookup("#macNCheese");
-        Button sideSaladButton = (Button) orderScreen.lookup("#sideSalad");
-        Button fruitPlateButton = (Button) orderScreen.lookup("#fruitPlate");
-        Button applesauceButton = (Button) orderScreen.lookup("#applesauce");
-        Button fountainDrinkButton = (Button) orderScreen.lookup("#fountainDrink");
-        Button lemonadeButton = (Button) orderScreen.lookup("#lemonade");
-        Button waterButton = (Button) orderScreen.lookup("#water");
-
-        cheeseburgerButton.setOnAction(e -> appendToOrderBox("Cheeseburger", specialInstructionsField, orderBox));
-        baconBurgerButton.setOnAction(e -> appendToOrderBox("Bacon Burger", specialInstructionsField, orderBox));
-        hamAndCheeseButton.setOnAction(e -> appendToOrderBox("Ham and Cheese", specialInstructionsField, orderBox));
-        chickenSandwichButton.setOnAction(e -> appendToOrderBox("Chicken Sandwich", specialInstructionsField, orderBox));
-        cobbSaladButton.setOnAction(e -> appendToOrderBox("Cobb Salad", specialInstructionsField, orderBox));
-        chickenTendersButton.setOnAction(e -> appendToOrderBox("Chicken Tenders", specialInstructionsField, orderBox));
-        straightFriesButton.setOnAction(e -> appendToOrderBox("Straight Fries", specialInstructionsField, orderBox));
-        curlyFriesButton.setOnAction(e -> appendToOrderBox("Curly Fries", specialInstructionsField, orderBox));
-        macNCheeseButton.setOnAction(e -> appendToOrderBox("Mac n Cheese", specialInstructionsField, orderBox));
-        sideSaladButton.setOnAction(e -> appendToOrderBox("Side Salad", specialInstructionsField, orderBox));
-        fruitPlateButton.setOnAction(e -> appendToOrderBox("Fruit Plate", specialInstructionsField, orderBox));
-        applesauceButton.setOnAction(e -> appendToOrderBox("Applesauce", specialInstructionsField, orderBox));
-        fountainDrinkButton.setOnAction(e -> appendToOrderBox("Fountain Drink", specialInstructionsField, orderBox));
-        lemonadeButton.setOnAction(e -> appendToOrderBox("Lemonade", specialInstructionsField, orderBox));
-        waterButton.setOnAction(e -> appendToOrderBox("Water", specialInstructionsField, orderBox));
+        for (String item : MENUITEMS) {
+            Button itemButton = (Button) orderScreen.lookup("#" + item);
+            itemButton.setOnAction(e -> appendToOrderBox(item, specialInstructionsField, orderBox));
+        }
     }
 
     private void appendToOrderBox(String foodItem, TextField specialInstructionsField, TextArea orderBox) {
